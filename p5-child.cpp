5ceb8601ac6f08f4e76eeec27513cfcc2664cde3
kimmerr9 <justin.kim2665@hotmail.com>
2025-11-07 21:46:46 -0500

    small changes

--- p5-child.cpp
+++ p5-child.cpp
@@ -12,10 +12,9 @@ int main(int argc, char* argv[]) {
     // get id from parent and attach to address space
     int shmID = std::atoi(argv[1]);
     int semID = std::atoi(argv[2]);              
-    void* addr = shmat(shmID, nullptr, 0);      
+    void* addr = shmat(shmID, NULL, 0);      
     if(addr == (void*)-1) { 
-        std::cout << "error when re-attaching child to the shared memory space" << std::endl; 
-        return 1; 
+        perror("Error in shmmat()");
     }
     Shared5* shm = static_cast<Shared5*>(addr);
 
@@ -23,29 +22,25 @@ int main(int argc, char* argv[]) {
     struct sembuf V = {0, +1, 0};
 
     if(semop(semID, &P, 1) < 0) { 
-        perror("semop P"); 
-        return 1; 
+        perror("Error semop P");  
     }
     if (shm->counter < 0) {
         shm->counter = 0;
     }
-    int m = shm->multiple;                 
+    int m = shm->multiple; 
     if(semop(semID, &V, 1) < 0) { 
-        perror("semop V"); 
-        return 1; 
+        perror("Error semop V"); 
     }
 
-    // wait for counter to get to 100
+    // wait for counter to be >= 100
     while(1) {
         int lock_count;
         if(semop(semID, &P, 1) < 0) { 
-            perror("semop P"); 
-            return 1; 
+            perror("Error semop P"); 
         }
         lock_count = shm->counter;
         if(semop(semID, &V, 1) < 0) { 
-            perror("semop V"); 
-            return 1; 
+            perror("Error semop V"); 
         }
         if(lock_count > 100) {
             break;
@@ -56,13 +51,11 @@ int main(int argc, char* argv[]) {
     while(1) {
         int c;
         if (semop(semID, &P, 1) < 0) { 
-            perror("semop P"); 
-            return 1; 
+            perror("Error semop P");
         }
         c = shm->counter;
         if (semop(semID, &V, 1) < 0) { 
-            perror("semop V"); 
-            return 1; 
+            perror("Error semop V");
         }
 
         if (c != last_seen) {
@@ -72,14 +65,13 @@ int main(int argc, char* argv[]) {
             last_seen = c;
         }
         if (c > 500) {
-            std::cout << "[Process2] observed counter= " << c << " which is > 500. Terminating.\n";
+            std::cout << "[Process2] observed counter= " << c << " which is > 500. Terminating" << std::endl;
             break;
         }
     }
 
     if(shmdt(addr) < 0) {
-        std::cout << "error when deattaching child to shared memory space" << std::endl;
-        return 1;
+        perror("error when detaching child to shared memory segment");
     }
     std::cout << "[Process2] - Terminated" << std::endl;
     return 0;
